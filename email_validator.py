4ab4ae7a8e13d62987cffcc91e1c62708723d524
Nishitha Singh N <nishithasingh777@gmail.com>
2024-02-17 18:14:06 +0530

    final

--- email_validator.py
+++ email_validator.py
@@ -1,21 +1,35 @@
 def email_verification(email):
-    # Check if the email contains "@" symbol
-    if "@" in email:
-        # Split the email address into local part and domain part
-        local_part, domain_part = email.split("@")
+    j,d,k=0,0,0    
+    if len(email)>=6:
+        if email[0].isalpha():
+            if ("@" in email) and (email.count("@")==1):
+                if(email[-4]==".") ^ (email[-3:]=="."):
+                    for i in email:
+                        if i==i.isspace():
+                            k=1
+                        elif i.isalpha():
+                            if i==i.upper():
+                                j=1
+                        elif i.isdigit():
+                            continue
+                        elif i =="_" or i =="." or i =="@":
+                            continue
+                        else:
+                            d=1
 
-        # Check if both parts are not empty
-        if local_part and domain_part:
-            # Check if the domain has at least one dot (.)
-            if "." in domain_part:
-                print(f"Email verification successful. {email} is a valid email address.")
+
+                    if k==1 or j==1 or d==1:
+                        print("wrong email")   
+                    else:
+                        print(f"Your email {email} is correct!!")         
+
+                else:
+                    print(f'Email must end with ".com" or ".org" or ".in".')
             else:
-                print(f"Invalid email. The domain in {email} is missing a dot (.)")
-        else:
-            print(f"Invalid email. Local part or domain part is empty in {email}")
+                print("wrong email")
     else:
-        print(f"Invalid email. {email} is missing the @ symbol.")
+        print("wrong email")
 
-# Example usage:
+
 user_email = input("Enter your email address: ")
 email_verification(user_email)
